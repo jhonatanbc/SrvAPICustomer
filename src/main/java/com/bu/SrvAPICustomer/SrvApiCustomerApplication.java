b036942ed174d629410cf4f49da0178272eeaa55
Jhonatan Bermudez <jhonatan.bermudez@bancounion.com>
2024-10-08 13:08:55 -0500

    Initial commit

---
+++ src/main/java/com/bu/SrvAPICustomer/SrvApiCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.bu.SrvAPICustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvApiCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvApiCustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bu/SrvAPICustomer/SrvApiCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bu.SrvAPICustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrvApiCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
